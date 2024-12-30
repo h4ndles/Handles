7cfdb0210ab72fd3708a2708b91aa76a3ad2a74f
Handles <drainage5000@gmail.com>
2024-12-30 15:13:58 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function loadSymbols() {
+  for (let start = 44032; start < 55215; start++) {
+    var div = document.createElement("div");
+    document.getElementById("clist").appendChild(div);
+    div.className = "char";
+    div.id = "char" + [i];
+    div.innerHTML =
+      '<h1>&#' + [start] + '</h1>';
+  }
+}
+
+loadSymbols();
