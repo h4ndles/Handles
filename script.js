64db856abeee12f85e880b8b448329689a71dc75
Handles <drainage5000@gmail.com>
2024-12-30 15:47:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function loadSymbols() {
   for (let start = 44032; start < 44132; start++) {
-    var Title = $('<textarea />').html("&#" + start).text();
-    var hex = Title.codePointAt(0).toString(16);
+    var hex = "hi".codePointAt(0).toString(16);
     var result = "\\u" + "0000".substring(0, 4 - hex.length) + hex;
     document.getElementById("clist").appendChild(div);
     div.className = "char";
