f37b5289217c58811ed4846df889052a9b316866
Handles <drainage5000@gmail.com>
2024-12-30 15:45:40 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function loadSymbols() {
   for (let start = 44032; start < 44132; start++) {
-    var div = document.createElement("div");
+    var Title = $('<textarea />').html("&#" + start).text();
+    var hex = Title.codePointAt(0).toString(16);
+    var result = "\\u" + "0000".substring(0, 4 - hex.length) + hex;
     document.getElementById("clist").appendChild(div);
     div.className = "char";
     div.id = "char" + [start];
     div.innerHTML =
-      '<h1>&#' + [start] + '</h1>';
+      '<h1>&#' + [start] + '</h1><p>' + result + '</p>';
   }
 }
 
