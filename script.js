b855c51f836454fbfe4f551ee32879a3c180c10c
Handles <drainage5000@gmail.com>
2024-12-30 15:51:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function loadSymbols() {
   for (let start = 44032; start < 44132; start++) {
-    var hex = "hi".codePointAt(0).toString(16);
-    var result = "\\u" + "0000".substring(0, 4 - hex.length) + hex;
+    var entity = "&#" + start;
+    var hex = entity.codePointAt(0).toString(16);
+    var result = "U+" + "0000".substring(0, 4 - hex.length) + hex;
     var div = document.createElement("div");
     document.getElementById("clist").appendChild(div);
     div.className = "char";
     div.id = "char" + [start];
     div.innerHTML =
-      '<h1>&#' + [start] + '</h1><p>' + result + '</p>';
+      '<h1>' + entity + '</h1><p>' + result + '</p>';
   }
 }
 
